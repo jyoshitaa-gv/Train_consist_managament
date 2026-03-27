4cd24649008fac3df2a098a2a68934276bd13ff4
jyoshitaa-gv <jyoshitaa9806@gmail.com>
2026-03-27 16:24:31 +0530

    UC5

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Train Consist Management App ");
-        LinkedList<String> train = new LinkedList<>();
+        LinkedHashSet<String> train = new LinkedHashSet<>();
         train.add("Engine");
         train.add("Sleeper");
-        train.add("AC");
         train.add("Cargo");
         train.add("Guard");
-        System.out.println("Initial Train: " + train);
-        train.add(2, "Pantry Car");
-        System.out.println("After adding Pantry Car: " + train);
-        train.removeFirst();
-        train.removeLast();
-        System.out.println("Final Train Consist: " + train);
+        train.add("Sleeper");
+        System.out.println("Train Formation: " + train);
+
     }
 }
