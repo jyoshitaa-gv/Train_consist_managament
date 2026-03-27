2a5893c3dc47922ec042c73380ea9249cb05e9aa
Jyoshitaa GV <jyoshitaa9806@gmail.com>
2026-03-27 15:56:44 +0530

    Refactor main method and add new class
    Refactor main method and add Train Consist Management App class.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-void main() {
-    IO.println(String.format("Hello and welcome!"));
-    for (int i = 1; i <= 5; i++) {
-        IO.println("i = " + i);
+import java.util.*;
+public class Trainconsistmanagementapp {
+    public static void main(String[] args) {
+        System.out.println(" Train Consist Management App ");
+        List<String> bogies = new ArrayList<>();
+        System.out.println("Initial Bogie Count: " + bogies.size());
     }
 }
