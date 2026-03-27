730ab269e6b8bf3f6b470f7f6f6d43593dcd03e6
jyoshitaa-gv <jyoshitaa9806@gmail.com>
2026-03-27 16:40:27 +0530

    UC8

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,35 @@
 import java.util.*;
-public class Main {
+import java.util.stream.Collectors;
+class Bogie {
+    String name;
+    int capacity;
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+    public String toString() {
+        return name + " - Capacity: " + capacity;
+    }
+}
+public class Trainconsistmanagementapp {
     public static void main(String[] args) {
         System.out.println(" Train Consist Management App ");
-        LinkedHashSet<String> train = new LinkedHashSet<>();
-        train.add("Engine");
-        train.add("Sleeper");
-        train.add("Cargo");
-        train.add("Guard");
-        train.add("Sleeper");
-        System.out.println("Train Formation: " + train);
-
+        List<Bogie> bogies = new ArrayList<>();
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 40));
+        bogies.add(new Bogie("General", 90));
+        System.out.println("\nAll Bogies:");
+        for (Bogie b : bogies) {
+            System.out.println(b);
+        }
+        List<Bogie> filteredBogies = bogies
+                .stream()
+                .filter(b -> b.capacity > 60)
+                .collect(Collectors.toList());
+        System.out.println("\nFiltered Bogies (Capacity > 60):");
+        for (Bogie b : filteredBogies) {
+            System.out.println(b);
+        }
     }
 }
