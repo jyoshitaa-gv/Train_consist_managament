b60eb287184637f3799f2b329918e27e5e88bf4c
jyoshitaa-gv <jyoshitaa9806@gmail.com>
2026-03-27 16:34:36 +0530

    UC6

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.util.*;
-public class Main {
+public class Main{
     public static void main(String[] args) {
         System.out.println(" Train Consist Management App ");
-        LinkedHashSet<String> train = new LinkedHashSet<>();
-        train.add("Engine");
-        train.add("Sleeper");
-        train.add("Cargo");
-        train.add("Guard");
-        train.add("Sleeper");
-        System.out.println("Train Formation: " + train);
+        HashMap<String, Integer> bogieCapacity = new HashMap<>();
+        bogieCapacity.put("Sleeper", 72);
+        bogieCapacity.put("AC Chair", 60);
+        bogieCapacity.put("First Class", 40);
+        bogieCapacity.put("Cargo", 100);
+        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+            System.out.println(entry.getKey() + " Capacity: " + entry.getValue());
+        }
 
     }
 }
