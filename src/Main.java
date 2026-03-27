eb6c6445ec20fc99580eed67ec8c4188710f888d
jyoshitaa-gv <jyoshitaa9806@gmail.com>
2026-03-27 16:09:18 +0530

    UC3

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Train Consist Management App ");
-        List<String> bogies = new ArrayList<>();
-        bogies.add("Sleeper");
-        bogies.add("AC Chair");
-        bogies.add("First Class");
-        System.out.println("Bogies after adding: " + bogies);
-        bogies.remove("AC Chair");
-        System.out.println("After removing AC Chair: " + bogies);
-        System.out.println("Is Sleeper present? " + bogies.contains("Sleeper"));
+        Set<String> bogieIds = new HashSet<>();
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG103");
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        System.out.println("Unique Bogie IDs: " + bogieIds);
     }
 }
