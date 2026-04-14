7833d1fd30907bf5ce7901bf2b4402df6b9d3001
jyoshitaa-gv <jyoshitaa9806@gmail.com>
2026-04-14 20:09:40 +0530

    UC9

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,40 @@
-<<<<<<< HEAD
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-=======
 import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(" Train Consist Management App ");
-        LinkedHashSet<String> train = new LinkedHashSet<>();
-        train.add("Engine");
-        train.add("Sleeper");
-        train.add("Cargo");
-        train.add("Guard");
-        train.add("Sleeper");
-        System.out.println("Train Formation: " + train);
 
->>>>>>> a24aeb5aed827e8035e6cba0839ed5da2275b7b0
+import java.util.stream.Collectors;
+class Bogie {
+    String name;
+    int capacity;
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+    public String toString() {
+        return name + " -> " + capacity;
+    }
+}
+public class Main {
+    public static void main(String args[]){
+        System.out.println(" Train Consist Management App ");
+        List<Bogie> bogies = new ArrayList<>();
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 40));
+        bogies.add(new Bogie("General", 90));
+        bogies.add(new Bogie("Sleeper", 72)); // duplicate type for grouping demo
+        bogies.add(new Bogie("AC Chair", 56));
+        System.out.println("\nAll Bogies:");
+        for (Bogie b : bogies) {
+            System.out.println(b);
+        }
+        Map<String, List<Bogie>> groupedBogies = bogies
+                .stream()
+                .collect(Collectors.groupingBy(b -> b.name));
+        System.out.println("\nGrouped Bogies By Type:");
+        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
+            System.out.println("\nBogie Type: " + entry.getKey());
+            for (Bogie b : entry.getValue()) {
+                System.out.println("  " + b);
+            }
+        }
     }
 }
