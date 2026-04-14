ce88e76f1747f5cd91c2a32faaf234d2eaefdef3
jyoshitaa-gv <jyoshitaa9806@gmail.com>
2026-04-14 21:31:41 +0530

    UC17

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,21 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+
 public class Main{
-    public static void bubbleSort(int[] capacities) {
-        int n = capacities.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j]= capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
+    public static void sortBogieNames(String[] bogieNames) {
+        Arrays.sort(bogieNames);
     }
 
     public static void main(String[] args) {
-        int[] capacities = {72, 56, 24, 70, 60};
+        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
 
-        System.out.println("Original Capacities:");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
-        bubbleSort(capacities);
+        System.out.println("Original Bogie Names:");
+        System.out.println(Arrays.toString(bogieNames));
+        sortBogieNames(bogieNames);
 
-        System.out.println("\n\nSorted Capacities (Ascending):");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println("\nSorted Bogie Names: ");
+        System.out.println(Arrays.toString(bogieNames));
+
     }
 }
